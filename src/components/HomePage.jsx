87e097ddb30f7178d7b7ae0d07bea40af90b38fd
ritwiksadhu <116791181+ritwiksadhu@users.noreply.github.com>
2023-05-18 11:13:05 +0530

    Style: filtering buttons

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductCategories from './product-categories/ProductCategories'
+import CarouselComponent from './carousel/CarouselComponent'
+
+const HomePage = () => {
+  return (
+    <>
+    <ProductCategories/>
+    <CarouselComponent/>
+    </>
+  )
+}
+
+export default HomePage
